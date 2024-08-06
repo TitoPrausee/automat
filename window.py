24dd4b001f5cba4fdc480d2ff25fd910e7c250ca
Hiron Tez <81362525+HironTez@users.noreply.github.com>
2024-08-06 08:59:26 +0200

    init window

---
+++ window.py
@@ -0,0 +1,14 @@
+# Import Module
+from tkinter import *
+
+# create root window
+root = Tk()
+
+# root window title and dimension
+root.title("Automat")
+# Set geometry (widthxheight)
+root.geometry('700x1000')
+
+# all widgets will be here
+# Execute Tkinter
+root.mainloop()
